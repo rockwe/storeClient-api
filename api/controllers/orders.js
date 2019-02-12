ce4fbe91bb2e35abbb09665c63cc26a0804ed511
Unknown <momorockwell@gmail.com>
2019-02-12 12:32:42 +0100

    add orders and order

---
+++ api/controllers/orders.js
@@ -0,0 +1,100 @@
+const Order = require('../models/order');
+
+
+exports.fetch =(req, res, next) => {
+    Order.find()
+        .exec()
+        .then(docs => {
+            res.status(200).json({
+                count: docs.length,
+                order: docs.map(doc => {
+                    return {
+                        user: doc.user,
+                        code: doc.code,
+                        line_basket: doc.line_basket,
+                        created_at: doc.created_at,
+                        updated_at: doc.updated_at,
+                        request: {
+                            type: "GET",
+                            url: "http://localhost:5000/api/v1/order/" + doc._id
+                        }
+                    };
+                })
+            });
+        })
+        .catch(err => {
+            res.status(500).json({
+                error: err
+            });
+        });
+};
+
+exports.find = (req, res, next) => {
+    Order.findById(req.params.id)
+        .exec()
+        .then(order => {
+            if (!order) {
+                return res.status(404).json({
+                    message: "commander  not found"
+                });
+            }
+            res.status(200).json({ data: order });
+        })
+        .catch(err => {
+            res.status(500).json({
+                error: err
+            });
+        });
+};
+
+exports.create = (req, res, next) => {
+    const order = new Order({
+        user: req.body.user,
+        line_basket: req.body.line_basket
+    });
+    order.save()
+        .then(result => {
+            console.log(result);
+            res.status(201).json({
+                message: "Success",
+                data: {
+                    _id: result._id,
+                    user: result.user,
+                    line_basket: result.line_basket
+                }
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            });
+        });
+};
+exports.delete = (req, res, next) => {
+    Order.remove({ _id: req.params.id })
+        .exec()
+        .then(result => {
+            res.status(200).json({
+                message: "Succes"
+            });
+        })
+        .catch(err => {
+            res.status(500).json({
+                error: err
+            });
+        });
+};
+
+exports.patch = (req, res, next) => {
+
+    Order.findByIdAndUpdate(req.params.id, {
+        isvalid: req.body.isvalid,
+        updated_at: req.body.updated_at
+    }, {new: true}, function (err) {
+        if (err) {
+            res.send({state: "erreur update order"})
+        }
+        res.send({state: "Success"})
+    })
+};
--- api/models/order.js
+++ api/models/order.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const orderSchema = mongoose.Schema({
     user: { type: mongoose.Schema.Types.ObjectId, required: true, ref:'User'},
-    product: { type: mongoose.Schema.Types.ObjectId, required: true, ref:'Product'},
+    line_basket: { type: mongoose.Schema.Types.ObjectId, required: true, ref:'LineBasket'},
     isvalid: { type: Boolean, required: true, default: false },
     created_at: { type: Date, default: Date.now },
     updated_at: { type: Date, default: Date.now }
