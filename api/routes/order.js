af3f67c3149e5f4c275257ff0447fae39d167cbc
Unknown <momorockwell@gmail.com>
2019-03-15 16:43:55 +0100

    mirror fix

--- api/routes/order.js
+++ api/routes/order.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const checkAuth = require('../middleware/chack-auth');
-constrouter = express.Router();
+const router = express.Router();
 
 const orderController = require('../controllers/orders');
 
-//router.get('/',checkAuth, orderController.fetch);
+router.get('/',checkAuth, orderController.fetch);
 router.get('/:id',checkAuth, orderController.find);
 router.post('/create',checkAuth, orderController.create);
 router.delete('/:id',checkAuth, orderController.delete);
