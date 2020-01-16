406a542be95dfa9a99562465cc498208751d0587
rock well <momorockwell@gmail.com>
2020-01-16 23:57:12 +0100

    add ecosystem

---
+++ ecosystem.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    apps : [{
+      name: "app",
+      script: "./server.js",
+      env: {
+        NODE_ENV: "development",
+      },
+      env_production: {
+        NODE_ENV: "production",
+      }
+    }, {
+       name: 'worker',
+       script: 'worker.js'
+    }]
+  }
